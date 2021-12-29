3d2fb7a4867c10e35c5c5b37d9320dc2007d94c6
JosueFrancoB <gerardo@cbin.mx>
2021-12-28 23:42:51 -0600

    Login and Register 0.0.1

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NbAuthModule } from '@nebular/auth';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { MainComponent } from './pages/main/main.component';
+import { NbAlertModule, NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule, NbLayoutModule } from '@nebular/theme';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    NbCardModule,
+    NbLayoutModule,
+    NbFormFieldModule,
+    NbInputModule,
+    NbIconModule,
+    NbAuthModule,
+    NbAlertModule,
+    NbButtonModule, 
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
