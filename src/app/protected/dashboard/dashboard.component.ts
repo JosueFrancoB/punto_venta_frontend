59086dbf6cd1757ffdb8cb2a7e02e5b2d50afca7
JosueFrancoB <gerardo@cbin.mx>
2022-07-05 01:11:57 -0500

    statistics working

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -18,6 +18,10 @@ export class DashboardComponent implements OnInit {
   rich_clients:Array<any> = []
   frecuency_clients:Array<any> = []
   current_week:any
+  date_key: string = 'week'
+  case_client: number = 1
+  case_employee: number = 1
+
   ngOnInit() {
     this.current_week = this.ProcessDate(this.getFullDay(), 'week')
     this.getStatistics('all', 'week', this.current_week)
@@ -40,6 +44,12 @@ export class DashboardComponent implements OnInit {
     let startDate:any = new Date(currentDate.getFullYear(), 0, 1);
     let days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
     return Math.ceil(days / 7)
+  }
+  toggleClient(){
+    this.case_client = this.case_client == 1 ? 2 : 1
+  }
+  toggleEmployee(){
+    this.case_employee = this.case_employee == 1 ? 2 : 1
   }
 
   getFullDay(){
@@ -70,12 +80,63 @@ export class DashboardComponent implements OnInit {
     this.statisticsService.getStatistics(statistic, date_key, date)
       .subscribe(res =>{
         console.log(res);
-        this.most_selled_products = res.statistics.most_selled_products
-        this.seller_employees = res.statistics.seller_employees
-        this.money_employees = res.statistics.money_employees
-        this.rich_clients = res.statistics.rich_clients
-        this.frecuency_clients = res.statistics.frecuency_clients
+        switch (statistic) {
+          case 'all':
+            this.most_selled_products = res.statistics.most_selled_products
+            this.seller_employees = res.statistics.seller_employees
+            this.money_employees = res.statistics.money_employees
+            this.rich_clients = res.statistics.rich_clients
+            this.frecuency_clients = res.statistics.frecuency_clients
+            break;
+          case 'most_selled_products':
+            this.most_selled_products = res.statistics.most_selled_products
+            break;
+          case 'seller_employees':
+            this.seller_employees = res.statistics.seller_employees
+            break;
+          case 'money_employees':
+            this.money_employees = res.statistics.money_employees
+            break;
+          case 'rich_clients':
+            this.rich_clients = res.statistics.rich_clients
+            break;
+          case 'frecuency_clients':
+            this.frecuency_clients = res.statistics.frecuency_clients
+            break;
+          default:
+            break;
+        }
       })
+  }
+
+  changeDate(statistic:string){
+    let date = ''
+    switch (this.date_key) {
+      case 'week':
+        this.date_key = 'month'
+        date = this.ProcessDate(this.getFullDay(), this.date_key)
+        this.getStatistics(statistic,this.date_key,date)
+        break;
+        case 'month':
+          this.date_key = 'year'
+          date = this.ProcessDate(this.getFullDay(), this.date_key)
+          this.getStatistics(statistic,this.date_key,date)
+        break;
+      case 'year':
+        this.date_key = 'day'
+        date = this.ProcessDate(this.getFullDay(), this.date_key)
+        this.getStatistics(statistic,this.date_key,date)
+        break;
+      case 'day':
+        this.date_key = 'week'
+        date = this.ProcessDate(this.getFullDay(), this.date_key)
+        this.getStatistics(statistic,this.date_key,date)
+        break;
+    
+      default:
+        break;
+    }
+    
   }
 
 }
